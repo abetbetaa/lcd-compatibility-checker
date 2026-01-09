f21aeaf3b11abd357c516002d7aa953e539d2ab2
abetbetaa <abetbetaa@gmail.com>
2026-01-09 22:05:01 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+// import.js
+export const lcdData = [
+    // REALME & OPPO (Very high compatibility)
+    { model: "Realme C21", compat: "Realme C21Y, C25, C25s, Oppo A16, Oppo A16s", image:"images/realmec21.2.png" },
+    { model: "Realme C11 (2021)", compat: "Realme C20, C11 2021, Narzo 50i, Oppo A15, A15s", image:"images/realmec11-2021.png" },
+    { model: "Realme 5", compat: "Realme 5i, 5s, 6i, C3, Narzo 10, Narzo 10A" },
+    
+    // XIAOMI / REDMI
+    { model: "Redmi Note 10 4G", compat: "Redmi Note 10S, Poco M5s (AMOLED versions)" },
+    { model: "Redmi 9", compat: "Redmi 9 Prime, Poco M2" },
+    { model: "Redmi 9A", compat: "Redmi 9C, Redmi 10A, Poco C3, Poco C31" },
+    { model: "Redmi Note 8", compat: "Redmi Note 8 (2019/2021 versions usually match)" },
+    { model: "Xiaomi Mi A3", compat: "Xiaomi CC9e" },
+
+    // SAMSUNG (A-Series often has many variants)
+    { model: "Samsung Galaxy A51", compat: "SM-A515F, SM-A515G, SM-A515U (Check Frame/No Frame)" },
+    { model: "Samsung Galaxy A10s", compat: "SM-A107F, SM-A107M" },
+    { model: "Samsung Galaxy A12", compat: "A12 Nacho, M12, A125F, A127F" },
+    { model: "Samsung Galaxy A02", compat: "Samsung M02, A022F" },
+    { model: "Samsung Galaxy J6 Plus", compat: "J4 Plus, J415, J610" },
+
+    // VIVO
+    { model: "Vivo Y20", compat: "Y20i, Y20s, Y12s, Y15s, Y21 (2021), Y30, Y50" },
+    { model: "Vivo Y91", compat: "Y93, Y95, Y91i, Y91c" },
+
+    // INFINIX / TECNO (Often use the same screens)
+    { model: "Infinix Hot 10", compat: "Infinix Note 7 Lite, X682, X682B" },
+    { model: "Infinix Smart 5", compat: "Tecno Spark 6 Go, Spark 5 Air" },
+    { model: "Tecno Camon 15", compat: "Infinix S5 Pro" },
+
+    // NOKIA
+    { model: "Nokia C21 Plus", compat: "TA-1433, TA-1431, TA-1424" },
+    { model: "Nokia 2.3", compat: "Nokia 2.2 (Some variants), TA-1206" }
+];
